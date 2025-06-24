f56414ba4fa6c2ae36681aa40c7625771bec8356
chen <1617383757@qq.com>
2025-06-24 18:05:25 +0800

    Update build_python-ldap.cmd

--- build_python-ldap.cmd
+++ build_python-ldap.cmd
@@ -3,8 +3,9 @@
 setlocal
 
 :: set PYTHONLDAP_VER=python-ldap-build-windows-3.4.4
+:: set PYTHONLDAP_RAW=python-ldap-3.4.4
 
-curl -L -o %PYTHONLDAP_VER%.tar.gz https://github.com/python-ldap/python-ldap/archive/refs/tags/%PYTHONLDAP_VER%.tar.gz
+curl -L -o %PYTHONLDAP_VER%.tar.gz https://github.com/python-ldap/python-ldap/archive/refs/tags/%PYTHONLDAP_RAW%.tar.gz
 if errorlevel 1 exit /B 1
 
 tar -xf %PYTHONLDAP_VER%.tar.gz --strip-components=1
